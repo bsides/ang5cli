622e5f6bbc8556050b500451087ffb1e4439d044
Rafael BSIDES Pereira <rafaelbsides@gmail.com>
2017-11-13 21:48:43 -0200

    Lots of examples mage, ready to show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  whatever = true;
+  private _items = [
+    {
+      url: 'https://angular.io/tutorial',
+      title: 'Tour of Heroes'
+    },
+    {
+      url: 'https://github.com/angular/angular-cli/wiki',
+      title: 'CLI Documentation'
+    },
+    {
+      url: 'https://blog.angular.io/',
+      title: 'Angular blog'
+    }
+  ];
+  public items = [];
+
+  constructor() {
+    this.items = this._items.slice();
+  }
+
+  public toggleWhatever(event: Event) {
+    console.log(event);
+    console.log(event.target);
+    this.whatever = !this.whatever;
+  }
+  public removeItem(event: Event) {
+    this.items.pop();
+  }
+  public restoreItems() {
+    this.items = this._items.slice();
+  }
 }
